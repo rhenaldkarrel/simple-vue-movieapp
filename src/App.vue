f6214ce1b030aaa9e616f6cbd903470b4624c05e
Rhenald <renaldkarel@gmail.com>
2023-04-20 11:34:14 +0700

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+</style>
